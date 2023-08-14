afcac7fb5a331bff716e07aa76ae2e9f3a567b95
Juan <juanarangozapata35@gmail.com>
2023-08-14 16:43:54 -0500

    proyecto terminado

--- script.js
+++ script.js
@@ -23,3 +23,15 @@ buttons.forEach((item) => {
     }
   };
 });
+
+
+const themeToggleBtn= document.querySelector(".theme-toggler")
+const calculator=document.querySelector(".calculadora")
+const toggleIcon=document.querySelector("toggler-icon")
+let isDark=true;
+
+themeToggleBtn.onclick=()=>{
+  calculator.classList.toggle("dark")
+  themeToggleBtn.classList.toggle("active")
+  isDark=!isDark
+}
